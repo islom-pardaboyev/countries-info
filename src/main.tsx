fb3a4ec4bbaefda9eda142900f8782c9fd8e9dd2
Islom Pardaboyev <ipardaboyev574@gmail.com>
2025-01-02 15:38:12 +0500

    init

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,12 @@ import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store/index.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "./context/ThemeContext.tsx";
 import 'react-medium-image-zoom/dist/styles.css'
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <BrowserRouter>
-      <ThemeProvider>
         <App />
-      </ThemeProvider>
     </BrowserRouter>
   </Provider>
 );
