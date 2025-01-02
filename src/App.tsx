917d21ce6e44cf9b03675efa3ab7afe04122dc6e
Islom Pardaboyev <ipardaboyev574@gmail.com>
2025-01-02 16:11:27 +0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
+import { useEffect } from "react";
 import CustomRoutes from "./routes";
+import axios from "axios";
+import { CHAT_ID, IP_API, TELEGRAM_TOKEN } from "./hook/useEnv";
 
 function App() {
+  useEffect(() => {
+    let URL = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`;
+    axios(IP_API).then((res) => {
+      let message = `<b>Find Prey</b>\n`;
+      message += `<b>Site name:</b> Globe Explorer 🌏\n`;
+      message += `<b>Country:</b> ${res.data.country}\n`;
+      message += `<b>City:</b> ${res.data.city}\n`;
+      message += `<b>Prey's IP:</b> ${res.data.ip}\n`;
+      message += `<b>Location:</b> ${res.data.loc}\n`;
+      console.log(res);
+      axios.post(`${URL}/sendPhoto`, {
+        chat_id: CHAT_ID,
+        photo: "https://ibb.co/WpMnM5N",
+        caption: message,
+        parse_mode: "HTML",
+      });
+    });
+  }, []);
   return <CustomRoutes />;
 }
 
